884c0d94efb280c7c5bf1f50c5ab547b562f242a
Erik Behrends <behrends@users.noreply.github.com>
2018-10-08 17:13:06 +0200

    Tableiste mit Icons und farbig gestalten

--- js/AppNavigator.js
+++ js/AppNavigator.js
@@ -1,9 +1,37 @@
+import React from 'react';
 import { createBottomTabNavigator } from 'react-navigation';
+import { Icon } from 'expo';
 
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 
-export default createBottomTabNavigator({
-  Home: HomeScreen,
-  Settings: SettingsScreen
-});
+export default createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        title: 'Freunde',
+        tabBarIcon: ({ tintColor }) => (
+          <Icon.Feather name="home" size={24} color={tintColor} />
+        )
+      }
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        title: 'Einstellungen',
+        tabBarIcon: ({ tintColor }) => (
+          <Icon.Feather name="settings" size={24} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: 'darkorange',
+      style: {
+        backgroundColor: 'aliceblue'
+      }
+    }
+  }
+);
