4e0a47f98a8cd6edf44bbd707ddf082fede6d3c8
Erik Behrends <behrends@users.noreply.github.com>
2018-10-08 12:48:09 +0200

    Screens als eigene Komponenten in Unterordner verwalten

--- App.js
+++ App.js
@@ -1,36 +1,19 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { createBottomTabNavigator } from 'react-navigation';
 
-class Screen1 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>SCREEN1</Text>
-      </View>
-    );
-  }
-}
-
-class Screen2 extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>SCREEN2</Text>
-      </View>
-    );
-  }
-}
+import HomeScreen from './js/screens/HomeScreen';
+import SettingsScreen from './js/screens/SettingsScreen';
 
 const AppNavigator = createBottomTabNavigator({
-  Home: Screen1,
-  Settings: Screen2
+  Home: HomeScreen,
+  Settings: SettingsScreen
 });
 
 export default class App extends Component {
   render() {
     return (
-      <View style={{ flex: 1 }}>
+      <View style={styles.container}>
         <AppNavigator />
       </View>
     );
@@ -40,8 +23,6 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: '#fff'
   }
 });
