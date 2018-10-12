5792412285ad7382825c9cc5dc2238f2113dd26b
Erik Behrends <behrends@users.noreply.github.com>
2018-10-12 12:02:41 +0200

    Listeneintrag mit TouchableOpacity antippbar machen

--- js/components/FriendListItem.js
+++ js/components/FriendListItem.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function(props) {
   const { friend, onPress } = props;
-  return <Button title={`Gehe zu ${friend.name}`} onPress={onPress} />;
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <Text>{friend.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10
+  }
+});
