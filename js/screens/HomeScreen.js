67c6812e954a80a774bad29a0692b6f99e18af0e
Erik Behrends <behrends@users.noreply.github.com>
2018-10-15 16:09:53 +0200

    Laden der Daten mit State und ActivityIndicator vorbereiten

--- js/screens/HomeScreen.js
+++ js/screens/HomeScreen.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, View } from 'react-native';
 
 import FriendListItem from '../components/FriendListItem';
 
 export default class HomeScreen extends Component {
   static navigationOptions = { header: null };
 
+  state = { data: [], isLoading: true };
+
+  _fetchData = async () => {
+    const data = [
+      { first: 'Alice', last: 'Smith', email: 'test1@example.com' },
+      { first: 'Bob', last: 'Smith', email: 'test2@example.com' },
+      { first: 'Joe', last: 'Smith', email: 'test3@example.com' },
+      { first: 'Jane', last: 'Smith', email: 'test4@example.com' }
+    ];
+    // Simulation: 3 Sekunden
+    await new Promise(_ => setTimeout(_, 3000));
+    this.setState({ data, isLoading: false });
+  };
+
+  componentDidMount() {
+    this._fetchData();
+  }
+
   render() {
+    if (this.state.isLoading)
+      return (
+        <View style={styles.container}>
+          <ActivityIndicator size="large" color="darkorange" />
+        </View>
+      );
     return (
       <View style={styles.container}>
         <FlatList
-          data={[
-            { first: 'Alice', last: 'Smith', email: 'test1@example.com' },
-            { first: 'Bob', last: 'Smith', email: 'test2@example.com' },
-            { first: 'Joe', last: 'Smith', email: 'test3@example.com' },
-            { first: 'Jane', last: 'Smith', email: 'test4@example.com' }
-          ]}
+          data={this.state.data}
           keyExtractor={item => item.email}
           renderItem={({ item }) => (
             <FriendListItem
@@ -37,6 +56,7 @@ export default class HomeScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
     backgroundColor: '#fff',
     paddingTop: 30
   },
