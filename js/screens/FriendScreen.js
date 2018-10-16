4667cdf7c510c6ba8f47d1ca667dfb7da59d2e13
Erik Behrends <behrends@users.noreply.github.com>
2018-10-16 14:04:33 +0200

    Fotos aus dem Web laden und anzeigen

--- js/screens/FriendScreen.js
+++ js/screens/FriendScreen.js
@@ -10,14 +10,14 @@ export default class FriendScreen extends Component {
   };
 
   render() {
-    const navigation = this.props.navigation;
+    const friend = this.props.navigation.getParam('friend');
     return (
       <ScrollView
         style={styles.scrollview}
         contentContainerStyle={styles.container}
       >
-        <Image style={styles.image} source={require('../../assets/icon.png')} />
-        <Text>{navigation.getParam('friend').name.first}</Text>
+        <Image style={styles.image} source={{ uri: friend.picture.large }} />
+        <Text>{friend.name.first}</Text>
       </ScrollView>
     );
   }
